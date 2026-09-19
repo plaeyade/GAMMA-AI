86f2071310208fd9710a27ef65074d660b00202b
plaeyade <plaeyade@hotmail.com>
2026-09-19 13:48:59 -0600

    feat(foundation): add packages/observability/gamma_observability/__init__.py

---
+++ packages/observability/gamma_observability/__init__.py
@@ -0,0 +1,4 @@
+from gamma_observability.logging import configure_logging, correlation_id_var
+from gamma_observability.tracing import configure_tracing
+
+__all__ = ["configure_logging", "configure_tracing", "correlation_id_var"]
