436d13458baab2f2b29e584b1037ec8dcbf55421
plaeyade <plaeyade@hotmail.com>
2026-09-19 13:49:03 -0600

    feat(foundation): add packages/domain/gamma_domain/__init__.py

---
+++ packages/domain/gamma_domain/__init__.py
@@ -0,0 +1,23 @@
+"""Domain model exports."""
+
+from gamma_domain.models import (
+    Artifact,
+    Document,
+    DocumentVersion,
+    GovernanceClassification,
+    IngestionJob,
+    IngestionStatus,
+    Source,
+    SourceType,
+)
+
+__all__ = [
+    "Artifact",
+    "Document",
+    "DocumentVersion",
+    "GovernanceClassification",
+    "IngestionJob",
+    "IngestionStatus",
+    "Source",
+    "SourceType",
+]
